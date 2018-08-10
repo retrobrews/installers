cbffa44c138659b3d6798b1f371b9f09ec63002b
retrobrews <retrobrews@gmail.com>
2018-08-10 15:29:14 +0200

    auto-script

--- nes-roms.sh
+++ nes-roms.sh
@@ -60,7 +60,7 @@ rm gamelist.xml
 mv /home/pi/RetroPie/roms/nes/gamelist.xml /home/pi/.emulationstation/gamelists/nes
 
 cd /home/pi
-rm snes-rom-installer.sh
+rm snes-roms.sh
 echo " GAMELIST UPDATE ... done."
 sleep 2
 clear
