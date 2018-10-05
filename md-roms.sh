3f906e738e93a7942adb4431f6a4dbe84f0284a0
retrobrews <retrobrews@gmail.com>
2018-10-05 15:24:32 +0200

    auto-script

---
+++ md-roms.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+###
+###     Auto Installer of free Homebrew ROMS for Retropie/Emulationstation!
+###     !LEGALLY!
+###     Accepted File Extensions: .7z .bin .gen .md .sg .smd .zip
+###
+clear
+echo ""
+echo "======================================================================="
+echo "Welcome to Games Auto Installer for RetroPie (Emulationstation)!"
+echo "This package contains only free -> LEGAL GAMES!"
+echo "Copyright goes to the owner of the particular game."
+echo "Last update: 5th October 2018"
+echo ""
+echo "#######################################################################"
+echo "WARNING!! This installer will remove existing ROMs & GAMELISTS!"
+echo "#######################################################################"
+echo ""
+echo "List of all Homebrew games, which will be installed for Sega MegaDrive/"
+echo "Genesis System can be found on: https://github.com/retrobrews/md-games"
+
+echo ""
+echo "Please wait until end of installation."
+echo "Emulation station will be started automatically at the end."
+echo "======================================================================="
+echo ""
+read -n 1 -s -p "Press any key to continue...     OR Ctrl+C to stop operation"
+echo ""
+clear
+
+
+echo ""
+echo "======================================================================="
+echo "Downloading and Extracting Games to ROM directory, please wait..."
+echo "======================================================================="
+echo ""
+sleep 2
+
+
+cd /home/pi/RetroPie/roms
+if [ ! -d "megadrive" ]; then
+mkdir megadrive
+fi
+cd megadrive
+###CLEAN EVERYTHING!
+rm -rf *.*
+
+
+### GAMES
+wget --no-check-certificate -q -O master.zip 'https://codeload.github.com/retrobrews/md-games/zip/master'
+unzip master.zip
+mv -v md-games-master/* /home/pi/RetroPie/roms/megadrive > /dev/null
+rm -rf md-games-master
+rm *.zip
+clear
+
+echo ""
+echo "======================================================================="
+echo "Downloading and Extracting Games to ROM directory, please wait..."
+echo "======================================================================="
+echo ""
+echo " DOWNLOAD ... done."
+sleep 2
+
+echo ""
+echo "======================================================================="
+echo "Updating gamelist for Sega MegaDrive/Genesis"
+echo "======================================================================="
+echo ""
+sleep 2
+cd ~
+cd /home/pi/.emulationstation/gamelists/
+if [ ! -d "megadrive" ]; then
+mkdir megadrive
+fi
+cd megadrive
+rm gamelist.xml
+mv /home/pi/RetroPie/roms/megadrive/gamelist.xml /home/pi/.emulationstation/gamelists/megadrive > /dev/null
+sleep 2
+
+cd /home/pi
+rm md-roms.sh
+clear
+echo ""
+echo "======================================================================="
+echo "Updating gamelist for Sega MegaDrive/Genesis"
+echo "======================================================================="
+echo ""
+echo " GAMELIST UPDATE ... done."
+sleep 2
+clear
+
+echo ""
+echo "======================================================================="
+echo "Installation is Complete..."
+echo "======================================================================="
+echo ""
+echo ""
+echo "This script was not written by developers of RetroPie, Emulationstation"
+echo "or any Hobrew/Free game"
+echo "For further support on this script please email to retrobrews@gmail.com"
+echo "======================================================================="
+echo "BIG THANKS GOING to: "
+echo "various sources over the internet for the awesome games & covers!!"
+echo "======================================================================="
+echo ""
+echo "Enjoy the new games. Now script will run Emulationstation, please wait."
+echo ""
+echo ""
+sleep 15
+emulationstation
